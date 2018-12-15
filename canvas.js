e4836de34d4f43846e05907419f30e6983fa626b
Kevin <klinstrim@gmail.com>
2018-12-15 10:33:52 -0500

    moving square has height, 2 planes

--- canvas.js
+++ canvas.js
@@ -17,17 +17,48 @@ let gridWidth = GRID_WIDTH*GRID_COLS*2;
 let gridCenterY = canvas.height/2;
 let gridCenterX = (canvas.width-gridWidth)/2;
 
+// IMAGES
+let house = new Image();
+house.src = 'house.png';
+
 // CLOCK
 let gameClock = performance.now();
 let deltaTime;
 
+function transform1(){
+  ctx.transform(1, -.5, 1, .5, gridCenterX, gridCenterY);
+}
+function transform2(dx,dy){
+  ctx.transform(1, 0, -1, 1, dx+20, dy);
+}
+function transform3(dx,dy){
+  ctx.transform(1, -1, 0, 1, dx, dy);
+}
+
+
 // DRAW INSTRUCTIONS FOR ISOMETRIC GRID SQUARES
 function drawIsometricRectangle(x,y,width,height,color){
   ctx.fillStyle = color;
-  ctx.transform(1, -.5, 1, .5, gridCenterX, gridCenterY);
+  transform1();
   ctx.fillRect(x,y,width,height);
   ctx.resetTransform();//does this reset fillstyle?
 }
+
+// DRAW INSTRUCTIONS FOR ISOMETRIC GRID SQUARES Z
+function drawIsometricRectangleZ(x,y,width,height,color,dx,dy){
+  ctx.fillStyle = color;
+  // left side
+  transform1();
+  transform3(dx,dy);
+  ctx.fillRect(x,y,width*3,height);
+  // right side
+  ctx.resetTransform();
+  transform1();
+  transform2(dx,dy);
+  ctx.fillRect(x,y+20,width,height*(-3));
+  ctx.resetTransform();
+}
+
 
 // DRAW INSTRUCTIONS FOR REGULAR GRID SQUARES
 function drawRectangle(x,y,width,height,color){
@@ -89,7 +120,53 @@ class CanvasIsometricMap {
       }
     }
   }
-  get top() {return GRID_HEIGHT*GRID_ROWS/2}
+}
+
+// ISOMETRIC MAP CLASS 'Z'
+class CanvasIsometricMapZ {
+  constructor(obstacleColor, safeColor, gridNo,dx,dy) {
+    this.obstacleColor = obstacleColor;
+    this.safeColor = safeColor;
+    this.gridNo = gridNo;
+    this.dx = dx;
+    this.dy = dy;
+    // BUILD THE MAP ARRAY (replaces grid0 constant)
+    this.load = function() {
+      for (let i=0; i<GRID_COLS; i++) {
+        this.gridNo[i] = (this.gridNo[i] || []);
+        for (let j=0; j<GRID_ROWS; j++) {
+          if(this.gridNo[i][j]=='obstacle'){}
+          else{this.gridNo[i][j] = 'safe'}
+        }
+      }
+    }
+    // DRAW MAP TO SCREEN
+    this.update = function() {
+      // underlayer for grid lines
+      drawIsometricRectangleZ(0,0,GRID_WIDTH*GRID_COLS,GRID_HEIGHT*GRID_ROWS,'rgb(180,180,0'),this.dx,this.dy;
+      // create grid array
+      for(let i=0; i < GRID_COLS; i++){
+        for(let j=0; j < GRID_ROWS; j++) {
+          if(this.gridNo[i][j] =='obstacle'){
+            drawIsometricRectangleZ([i]*GRID_WIDTH, [j]*GRID_HEIGHT, GRID_WIDTH-GRID_GAP, GRID_HEIGHT-GRID_GAP, this.obstacleColor, this.dx, this.dy);
+          } 
+          else /*(grid[i][j] == 'Safe')*/{
+            drawIsometricRectangleZ([i]*GRID_WIDTH, [j]*GRID_HEIGHT, GRID_WIDTH-GRID_GAP, GRID_HEIGHT-GRID_GAP, this.safeColor, this.dx, this.dy);
+          }          
+        }      
+      }
+    }
+    // CLEAR MAP (unused)
+    this.clear = function() {
+      // change this to a .forEach
+      for (let i=0; i<GRID_COLS; i++) {
+        this.gridNo[i] = (this.gridNo[i] || []);
+        for (let j=0; j<GRID_ROWS; j++) {
+          this.gridNo[i][j] = 'safe'
+        }
+      }
+    }
+  }
 }
 
 // REGULAR MAP CLASS
@@ -139,8 +216,8 @@ class CanvasMap {
 }
 
 // CREATE ISOMETRIC MAP WITH UNDERLAYER
-let isometricGrid = new CanvasIsometricMap('gray','yellow', grid0);
-// let isometricUnderGrid = new CanvasIsometricMap('gray','rgb(120,120,0)',grid0);
+let isometricGrid = new CanvasIsometricMap('gray','green', grid0);
+let isometricGridZ = new CanvasIsometricMapZ('gray', 'blue', grid0, 200, 0);
 
 // CREATE REGULAR MAP
 let grid = new CanvasMap('gray','rgba(255,255,0,.2)',grid0);
@@ -152,7 +229,6 @@ class Block {
     this.y = y;
     this.vel = .1; // this should change when motion is applied
     this.update = function(){
-      console.log(this.x);
       drawIsometricRectangle(this.x,this.y,GRID_WIDTH,GRID_HEIGHT,'blue');
       this.x < 0 ? this.x = 0 : {};
       this.x > gridWidth/2-GRID_WIDTH ? this.x = gridWidth/2-GRID_WIDTH : {};
@@ -173,8 +249,43 @@ class Block {
     }
   }
 }
-// CREATE MOVING BLOCK
-let blockie = new Block(0,20);
+
+// MOVING BLOCK CLASS Z
+class BlockZ {
+  constructor(x,y,dx,dy){
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.vel = .1; // this should change when motion is applied
+    this.update = function(){
+      drawIsometricRectangleZ(this.x,this.y,GRID_WIDTH,GRID_HEIGHT,'rgba(255,255,0,.8)',this.dx, this.dy);
+      this.dx < 0 ? this.dx = 0 : {};
+      this.dx > gridWidth/2-GRID_WIDTH ? this.dx = gridWidth/2-GRID_WIDTH : {};
+      this.dy < 0 ? this.dy = 0 : {};
+      this.dy > gridHeight-GRID_HEIGHT ? this.dy = gridHeight-GRID_HEIGHT : {};
+      if(canvas.keys && canvas.keys[39]){
+        // this.x += 1*(deltaTime*this.vel);
+        this.dx += 1*(deltaTime*this.vel);
+      }
+      if(canvas.keys && canvas.keys[37]){
+        this.dx -= 1*(deltaTime*this.vel);
+      }
+      if(canvas.keys && canvas.keys[38]){
+        this.dy -= 1*(deltaTime*this.vel);
+      }
+      if(canvas.keys && canvas.keys[40]){
+        this.dy += 1*(deltaTime*this.vel);
+      }
+    }
+  }
+}
+
+// CREATE MOVING BLOCK (floor part)
+let blockie = new Block(0,0);
+// CREATE MOVING BLOCK (upward left)
+let blockie2 = new BlockZ(0,0,0,0);
+let blockie3 = new BlockZ(0,0,0,0);
 
 // EVENT LISTENER FOR MOUSE COORDINATES
 canvas.addEventListener('mousemove', function(e){
@@ -197,12 +308,14 @@ window.onload = function(){
   // CREATE CANVAS
   document.body.insertBefore(canvas, document.body.childNodes[0]); 
   // CREATE GRIDS
-  isometricGrid.load();  
+  // isometricGrid.load(); 
+  // isometricGridZ.load(); 
   grid.load();
   // INITIATE DRAWING LOOP
   drawAll();
 }
 
+// DRAWING LOOP
 function drawAll(){
   requestID = requestAnimationFrame(drawAll);
   // MEASURE PERFORMANCE/TIME
@@ -218,5 +331,10 @@ function drawAll(){
   // UPDATE GRIDS
   // grid.update(); //comparison grid shows isometric grid untransformed.
   isometricGrid.update();
-  blockie.update();
+  // isometricGridZ.update();
+  // blockie.update();
+  // blockie2.update();
+  blockie3.update();
+  
 }
+
